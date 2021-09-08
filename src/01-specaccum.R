22f23677d69f02d3f1223cc9e4a5a5f7ff1e3396
Garland Xie <garlandxie@Garlands-MacBook-Air.local>
2021-09-08 15:38:08 -0400

    Initial commit

---
+++ src/01-specaccum.R
@@ -0,0 +1,44 @@
+# libraries ----
+library(here)
+library(tidyverse)
+library(vegan)
+library(janitor)
+
+# import ---
+df_raw <- read.csv(
+  here("data/original", "Costa_Rica_Species_Accumulation.csv"), 
+  stringsAsFactors = FALSE
+)
+
+# check packaging ---
+str(df_raw)
+head(df_raw, n = 10)
+tail(df_raw, n = 10)
+
+# clean ----
+
+df_tidy <- df_raw %>%
+  janitor::clean_names() %>%
+  tibble::column_to_rownames(var = "collection")
+
+# species accumulation curve: interpolation -----
+
+specc <- specaccum(
+  comm = df_tidy, 
+  method = "rarefaction"
+)
+
+# grab maximum expected richness
+max(specc$richness)
+
+# plot the SAC
+plot(specc)
+
+# species accumulation curve: extrapolation ----
+
+fitspecc <- fitspecaccum(
+  specc, "lomolino"
+)
+
+coef(fitspecc)
+predict(fitspecc)
